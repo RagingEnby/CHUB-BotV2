eded4fa066c7ce06ad0ebbfa6e63b69398b3a58d
RagingEnby <ragingenby@gmail.com>
2025-10-21 09:31:35 -0400

    Added simple base disnake bot

---
+++ constants.py
@@ -0,0 +1,13 @@
+import scrts
+
+
+OWNER_IDS: set[int] = {
+    447861538095759365,  # RagingEnby
+    697136515461152768,  # Vinush
+    230778630597246983,  # Thomas
+    758304690617712651,  # Foe
+}
+
+
+# load secrets
+BOT_TOKEN: str = scrts.BOT_TOKEN
--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-def main():
-    print(":3")
+import disnake
+from disnake.ext import commands
+
+import constants
+
+
+bot = commands.InteractionBot(
+    intents=disnake.Intents(
+        automod=False,
+        automod_configuration=False,
+        automod_execution=False,
+        bans=True,
+        dm_messages=True,
+        dm_reactions=False,
+        dm_typing=False,
+        emojis=True,
+        emojis_and_stickers=False,
+        guild_messages=True,
+        guild_reactions=False,
+        guild_scheduled_events=False,
+        guild_typing=False,
+        guilds=True,
+        integrations=False,
+        invites=False,
+        members=True,
+        message_content=True,
+        moderation=True,
+        presences=False,
+        voice_states=False,
+        webhooks=False,
+    ),
+    owner_ids=constants.OWNER_IDS,
+)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
 
 
 if __name__ == "__main__":
-    main()
+    bot.run(constants.BOT_TOKEN)
