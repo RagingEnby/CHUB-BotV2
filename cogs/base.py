c80302689c7fbfea76e9fe6f007670ff6e9b6880
RagingEnby <ragingenby@gmail.com>
2025-10-21 19:05:22 -0400

    began base cog

--- cogs/base.py
+++ cogs/base.py
@@ -1,3 +1,15 @@
 """BASE COG - NOT FOR USAGE"""
 
+from disnake.ext import commands
 
+
+class BaseCog(commands.Cog):
+    def __init__(self, bot: commands.InteractionBot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"[BaseCog] Logged in as {self.bot.user}")
+
+    async def close(self):
+        print("[BaseCog] Closing cog...")
