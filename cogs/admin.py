9648c37df5adadfe2eb865556090fd8890d5c4a1
RagingEnby <ragingenby@gmail.com>
2026-01-29 21:57:31 -0500

    STARTED on verification bypass code

--- cogs/admin.py
+++ cogs/admin.py
@@ -40,7 +40,7 @@ class AdminCog(commands.Cog):
         inter: disnake.AppCmdInter,
         ign: str = commands.Param(
             name="ign",
-            description="The Minecraft username to verify to the selected member (also accepts UUIDs)",
+            description="The Minecraft username or UUID to link the member to.",
             min_length=1,
             max_length=32,
             autocomplete=autocomplete.ign,
@@ -50,4 +50,39 @@ class AdminCog(commands.Cog):
             description="The member to verify the Minecraft account to",
         ),
     ):
-        await self.LinkingCog.verify(inter, ign, member)
+        await inter.response.defer()
+        await self.LinkingCog.hypixel_verify(inter, ign, member)
+
+    @admin.sub_command_group(
+        name="force", description="Force admin verification actions"
+    )
+    async def force(self, _: disnake.AppCmdInter):
+        return
+
+    @force.sub_command(
+        name="verify",
+        description="Forcefully verify a Minecraft account to a user without checking if they own the account. **USE VERY RARELY**",
+    )
+    async def bypass_verification(
+        self,
+        inter: disnake.AppCmdInter,
+        member: disnake.Member = commands.Param(
+            name="member",
+            description="The member to bypass the verification process for",
+        ),
+        ign: str = commands.Param(
+            name="ign",
+            description="The Minecraft username or UUID to link the member to.",
+            min_length=1,
+            max_length=32,
+            autocomplete=autocomplete.ign,
+        ),
+        reason: str = commands.param(
+            name="reason",
+            description="A detailed and valid reason for bypassing the verification process.",
+        ),
+    ):
+        player, _ = await asyncio.gather(
+            mojang.get_player(ign),
+            inter.response.defer(),
+        )
