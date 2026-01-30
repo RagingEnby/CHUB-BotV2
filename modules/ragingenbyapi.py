b05fb5634aa3d3bc76634cbec45354ac74887b50
RagingEnby <ragingenby@gmail.com>
2026-01-29 20:27:59 -0500

    added cache to ign stem searching

--- modules/ragingenbyapi.py
+++ modules/ragingenbyapi.py
@@ -1,14 +1,18 @@
 from modules import asyncreqs, mojang
 import constants
 
+stem_cache: dict[str, list[mojang.Player]] = {}
+
 
 async def search_ign_stem(stem: str) -> list[mojang.Player]:
     stem = stem.lower().strip()
     if not stem:
         return []
+    if stem in stem_cache:
+        return stem_cache[stem].copy()
     response = await asyncreqs.get(constants.IGN_STEM_URL.format(stem))
     if response.status_code != 200:
         print(f"Failed to search for ign stem {stem}: {response.status_code}")
         return []
-    data = response.json()
-    return [mojang.Player.from_dict(player) for player in data]
+    stem_cache[stem] = [mojang.Player.from_dict(player) for player in response.json()]
+    return stem_cache[stem]
